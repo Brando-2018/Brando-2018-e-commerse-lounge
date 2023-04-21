d2a7ac675708df36a6011b65963740547ce57491
John Kutch <jkutch2@yahoo.com>
2023-04-20 21:33:53 -0400

    seed products

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,7 +23,7 @@ db.once('open', async () => {
       name: 'Arturo Fuent Rothschild Natural',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'AF Rothschild Natural.jpg',
+      image: 'af-rothschild-natural.jpg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 500
@@ -32,44 +32,35 @@ db.once('open', async () => {
       name: 'Arturo Fuente Don Carlos Double Robusto',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'AF Don Carlos Double Robusto.jpg',
+      image: 'af-don-carlos-double-robusto.jpg',
       category: categories[0]._id,
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'Arturo Fuente Hemingway Short Story',
-      category: categories[1]._id,
+      name: 'AF Hemingway Short Story',
+      category: categories[0]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'AF Hemingway Short Story.jpg',
+        'Arturo Fuente great cigars',
+      image: 'af-hemingway-short-story.jpg',
       price: 7.99,
       quantity: 20
     },
     {
       name: 'Cohiba Macassar',
-      category: categories[1]._id,
+      category: categories[0]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'Cohiba Macassar.jpg',
+      image: 'cohiba-macassar.jpg',
       price: 3.99,
       quantity: 50
     },
     {
-      name: 'Cohiba Nicaragua N54',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'Cohiba Nicaragua N54.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
       name: 'Cohiba Puro Dominicana',
-      category: categories[2]._id,
+      category: categories[0]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'Cohiba Puro Dominicana.jpg',
+      image: 'cohiba-puro-dominicana.jpg',
       price: 399.99,
       quantity: 30
     },
