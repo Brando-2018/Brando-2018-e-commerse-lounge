a55c28f6cea13d5a5e1b43987e0aee1f01e28cba
Brando-2018 <116236454+Brando-2018@users.noreply.github.com>
2023-04-27 21:04:56 -0400

    hero updated

--- client/src/components/HomeHero/index.js
+++ client/src/components/HomeHero/index.js
@@ -5,15 +5,10 @@ import './style.css';
 function HomeHero () {
 
 
-    const description = "This is your very own all-inclusive website for your avid cigars needs and coinsurer essentials. With this website you’ll be able to buy cigars, humidors, hookahs, lighters, cutters, cases, ashtrays, and shirt!"; 
-
 return (
 <div className="HomeHero-container">
     <div>
         <img className="hero-image" src={myImage} alt="cigarlanding.jpg" />
-        <p>
-        {description}
-        </p>
     </div>
 </div>            
 );
